035c5aa51555b8ffdca9c72ea74e196a1ffb1c7d
max-kalganov <maksim.kalganov@gmail.com>
2025-08-12 15:54:29 +0300

    add a module for push notificationsˆ

---
+++ border_equeue_stats/telegram_bot/push_notifications.py
@@ -0,0 +1,11 @@
+"""Contains notifications implementation"""
+
+import logging
+from border_equeue_stats.constants import PUSH_NOTIFICATIONS_LOGGER_NAME
+
+logger = logging.getLogger(PUSH_NOTIFICATIONS_LOGGER_NAME)
+
+
+# TODO:
+#   - how many logs, logs total size
+#   - ...
